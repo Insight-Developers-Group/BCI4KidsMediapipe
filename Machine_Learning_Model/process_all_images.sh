a776bee3faf290c5a3a7af79059bd223df9e1953
Kaitlin <kaitlin.decf@gmail.com>
2021-11-16 00:17:53 -0700

    Everything is a smile

--- Machine_Learning_Model/process_all_images.sh
+++ Machine_Learning_Model/process_all_images.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-for filename in \\images\\smiling\\*; do
+
+for filename in "$1"/*; do
 	echo $filename
-    python pic_to_facemesh.py -i $filename -o csvs/smile1.csv
+    python3 pic_to_facemesh.py -i $filename -o csvs/neutral.csv
 done 
+
+# target=$1
+# let count=0
+# for f in "$target"/*
+# do
+#     echo $(basename $f)
+#     let count=count+1
+# done
+# echo ""
+# echo "Count: $count"
