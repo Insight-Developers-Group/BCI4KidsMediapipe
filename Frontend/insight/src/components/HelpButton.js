3eff39831a9e89dee0c657d4a1a3026d1624d09f
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2021-11-29 16:30:34 -0700

    Implemented components Button, MenuButton, HelpButton, ReloadButton, ModeSwitcher

--- Frontend/insight/src/components/HelpButton.js
+++ Frontend/insight/src/components/HelpButton.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import helpLogo from '../Resources/helplogo.svg';
 import PropTypes from 'prop-types'
 
 function HelpButton(props) {
+
+    const [isOpen, setisOpen] = React.useState(false)
+
+    function toggle() {
+        setisOpen(prevIsOpen => !isOpen)
+        if (isOpen)
+            console.log("Help Open")
+        else
+            console.log("Help Closed")
+    }
+
     return (
-        <div>
-            
+        <div className="helpIconBgrnd" onClick={toggle}>
+            <img className="help-icon" src={helpLogo} alt='Help' />
         </div>
     )
 }
--- Frontend/insight/src/components/ModeSwitcher.js
+++ Frontend/insight/src/components/ModeSwitcher.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import eyelogo from '../Resources/eyelogo.svg'
+import girllogo from '../Resources/girllogo.svg'
 import PropTypes from 'prop-types'
 
 function ModeSwitcher(props) {
+
+    const [trackingLabel, setTrackingLabel] = React.useState('| Face Tracking')
+
+    function changeLabelToFaceTracking() {                  /*flips the trackingLabel state from face tracking to eye tracking and vice versa */
+        setTrackingLabel(prevLabel => '| Face Tracking')
+    }
+
+    function changeLabelToEyeTracking() {
+        setTrackingLabel(prevLabel => '| Eye Tracking')
+    }
+
     return (
         <div>
-            
+            <div className='girlBgrnd' onClick={changeLabelToFaceTracking}>
+                <img className='girllogo' src={girllogo} />
+            </div>
+            <div className='eyeBgrnd' onClick={changeLabelToEyeTracking}>
+                <img className='eyelogo' src={eyelogo} />
+            </div>
+            <div className='trackingLabel'>
+                <p className='labelContent'>{trackingLabel}</p>
+            </div>
         </div>
     )
 }
