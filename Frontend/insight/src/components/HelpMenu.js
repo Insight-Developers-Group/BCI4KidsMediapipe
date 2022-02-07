fb9c8fde42865cfce43629a849093cdb13e957ea
Dylan Wheeler <dwheeler1127@gmail.com>
2022-02-06 21:42:12 -0700

    Added base functionality and some very initial css

--- Frontend/insight/src/components/HelpMenu.js
+++ Frontend/insight/src/components/HelpMenu.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import helpLogo from "../Resources/helplogo.svg";
+import crossIcon from "../Resources/crossicon.svg";
 
 function HelpMenu(props) {
     return (
         <div>
-            
+            <div className="help-menu-overlay" onClick={props.toggle}></div>
+            <div className="help-menu">
+                <img className='help-menu-close' src={crossIcon} onClick={props.toggle} alt='close help menu' />
+                <img className="help-menu-icon" src={helpLogo} alt="help menu icon" />
+                <h1 className='help-menu-title'>Help</h1>
+                <div className='help-menu-content'>
+
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default HelpMenu
