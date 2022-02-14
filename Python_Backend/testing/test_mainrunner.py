9958e69a525d4f8b27fc1d29e8972b97555ec8b0
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2022-02-13 20:43:09 -0700

    rounded out test cases

--- Python_Backend/testing/test_mainrunner.py
+++ Python_Backend/testing/test_mainrunner.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 import sys
 sys.path.insert(0, '../')
 
@@ -6,8 +7,10 @@ from PIL import Image
 import cv2 as cv
 import numpy
 
+import MainRunner
 from MainRunner import convert_image
 from MainRunner import process_image
+from AnswerGenerator import Answer
 
 class TestMainRunner(unittest.TestCase):
 
@@ -36,6 +39,9 @@ class TestMainRunner(unittest.TestCase):
         self.assertEqual(compare,True)
 
     def test_process_image_bad_face(self):
+        mock = Mock()
+        mock.side_effect = Exception("FacialDFGenerator: No face detected")
+        MainRunner.DFGenerator.FacialDFGenerator.generate_df = mock
         im = Image.open("test_resources/test_img2.jpg")
         open_cv_image = numpy.array(im)
         open_cv_image = open_cv_image[:,:,::-1].copy()
@@ -51,6 +57,9 @@ class TestMainRunner(unittest.TestCase):
         self.assertEqual(testval,1)
 
     def test_process_image_bad_iris(self):
+        mock = Mock()
+        mock.side_effect = Exception("IrisDFGenerator: No face detected")
+        MainRunner.DFGenerator.FacialDFGenerator.generate_df = mock
         im = Image.open("test_resources/test_img2.jpg")
         open_cv_image = numpy.array(im)
         open_cv_image = open_cv_image[:,:,::-1].copy()
@@ -65,9 +74,92 @@ class TestMainRunner(unittest.TestCase):
         
         self.assertEqual(testval,1)
 
-    def test_process_image_smile_face(self):
-        im = Image.open("test_resources/smile_1.jpg")
+    def test_process_image_yes_face(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.facial_state_generator = mock
+        MainRunner.facial_answer_generator = mock2
+        MainRunner.facial_answer_generator.determine_answer().return_value = Answer.YES
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
         open_cv_image = numpy.array(im)
         open_cv_image = open_cv_image[:,:,::-1].copy()
-        print(process_image(("FACE",open_cv_image)))
+        test = process_image(("FACE",open_cv_image))
+        
+        self.assertEqual(test(),Answer.YES)
 
+    def test_process_image_yes_iris(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.iris_state_generator = mock
+        MainRunner.iris_answer_generator = mock2
+        MainRunner.iris_answer_generator.determine_answer().return_value = Answer.YES
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
+        open_cv_image = numpy.array(im)
+        open_cv_image = open_cv_image[:,:,::-1].copy()
+        test = process_image(("IRIS",open_cv_image))
+        
+        self.assertEqual(test(),Answer.YES)
+
+    def test_process_image_no_face(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.facial_state_generator = mock
+        MainRunner.facial_answer_generator = mock2
+        MainRunner.facial_answer_generator.determine_answer().return_value = Answer.NO
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
+        open_cv_image = numpy.array(im)
+        open_cv_image = open_cv_image[:,:,::-1].copy()
+        test = process_image(("FACE",open_cv_image))
+        
+        self.assertEqual(test(),Answer.NO)
+
+    def test_process_image_no_iris(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.iris_state_generator = mock
+        MainRunner.iris_answer_generator = mock2
+        MainRunner.iris_answer_generator.determine_answer().return_value = Answer.NO
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
+        open_cv_image = numpy.array(im)
+        open_cv_image = open_cv_image[:,:,::-1].copy()
+        test = process_image(("IRIS",open_cv_image))
+        
+        self.assertEqual(test(),Answer.NO)
+
+    def test_process_image_undefined_face(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.facial_state_generator = mock
+        MainRunner.facial_answer_generator = mock2
+        MainRunner.facial_answer_generator.determine_answer().return_value = Answer.UNDEFINED
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
+        open_cv_image = numpy.array(im)
+        open_cv_image = open_cv_image[:,:,::-1].copy()
+        test = process_image(("FACE",open_cv_image))
+        
+        self.assertEqual(test(),Answer.UNDEFINED)
+
+    def test_process_image_undefined_iris(self):
+        mock = Mock()
+        mock2 = Mock()
+        mock3 = Mock()
+        MainRunner.iris_state_generator = mock
+        MainRunner.iris_answer_generator = mock2
+        MainRunner.iris_answer_generator.determine_answer().return_value = Answer.UNDEFINED
+        MainRunner.DFGenerator = mock3
+        im = Image.open("test_resources/test_img1.jpg")
+        open_cv_image = numpy.array(im)
+        open_cv_image = open_cv_image[:,:,::-1].copy()
+        test = process_image(("IRIS",open_cv_image))
+        
+        self.assertEqual(test(),Answer.UNDEFINED)
