8b45e7cc753b629e22de0b06092fdd68dc812401
Dylan Wheeler <dwheeler1127@gmail.com>
2022-03-17 01:53:55 -0600

    Some polishing tweaks: added new (custom) iris logos, made background solid again, added icon colour switching for mode switcher, slowed down waiting animation

--- Frontend/insight/src/components/ModeSwitcher.js
+++ Frontend/insight/src/components/ModeSwitcher.js
@@ -1,22 +1,31 @@
 import React from "react";
-import eyelogo from "../Resources/eyelogo.svg";
-import girllogo from "../Resources/girllogo.svg";
+import irisDark from "../Resources/iris-dark.svg";
+import irisLight from "../Resources/iris-light.svg";
+import faceDark from "../Resources/girllogo.svg";
+import faceLight from "../Resources/girllogo-light.svg";
 // import girllogolight from "../Resources/girllogo-light.svg";
 
 function ModeSwitcher(props) {
     const [trackingLabel, setTrackingLabel] = React.useState("Face Tracking");
+
+    const [eyeColor, setEyeColor] = React.useState(irisLight);
+    const [faceColor, setFaceColor] = React.useState(faceDark);
 
     function changeLabelToFaceTracking() {
         /*flips the trackingLabel state from face tracking to eye tracking and vice versa */
         setTrackingLabel("Face Tracking");
         props.changeMode("face");
         console.log("Changed to face tracking");
+        setEyeColor(irisLight);
+        setFaceColor(faceDark);
     }
 
     function changeLabelToEyeTracking() {
         setTrackingLabel("Eye Tracking");
         props.changeMode("eye");
         console.log("Changed to eye tracking");
+        setEyeColor(irisDark);
+        setFaceColor(faceLight);
     }
 
     return (
@@ -25,13 +34,13 @@ function ModeSwitcher(props) {
                 className={"girlBgrnd " + props.mode + "-selected-face-bg"}
                 onClick={changeLabelToFaceTracking}
                 data-testid="girlBgrnd">
-                <img className="girllogo" src={girllogo} alt="girllogo" />
+                <img className="girllogo" src={faceColor} alt="girllogo" />
             </div>
             <div
                 className={"eyeBgrnd " + props.mode + "-selected-eye-bg"}
                 onClick={changeLabelToEyeTracking}
                 data-testid="eyeBgrnd">
-                <img className="eyelogo" src={eyelogo} alt="eyelogo" />
+                <img className="eyelogo" src={eyeColor} alt="eyelogo" />
             </div>
             <p className="labelContent" data-testid="para">
                 {trackingLabel}
