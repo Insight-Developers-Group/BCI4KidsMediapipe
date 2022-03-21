e83bbeb38a85b65fb5aad6a2335baf5b3b8bc68f
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-20 20:28:56 -0600

    Added meaningful error messages

--- Frontend/insight/src/components/ErrorResponse.js
+++ Frontend/insight/src/components/ErrorResponse.js
@@ -11,25 +11,25 @@ export default function ErrorResponsed(props) {
         else {
             errCmp.current.style.display = 'block';
             if (props.msg === "error: invalid state exception") {
-                errCmp.current.innerHTML = 'invalid_state_exception';
+                errCmp.current.innerHTML = "Oops! We couldn't validate that state";
             }
             else if (props.msg === "error: no face detected") {
-                errCmp.current.innerHTML = 'no_face_detected_exception'
+                errCmp.current.innerHTML = "Oops! We can't see you"
             }
             else if (props.msg === "error: multiple faces detected") {
-                errCmp.current.innerHTML = 'multi_face_detected_exception'
+                errCmp.current.innerHTML = "Oops! too many people in the frame"
             }
             else if (props.msg === "error: invalid model type") {
-                errCmp.current.innerHTML = 'invalid_model_type'
+                errCmp.current.innerHTML = "Oops! we couldn't validate that model"
             }
             else if (props.msg === "error: df generator failed") {
-                errCmp.current.innerHTML = 'df_generator_exception'
+                errCmp.current.innerHTML = "Oops! DF generator failed"
             }
             else if (props.msg === "error: state generator failed") {
-                errCmp.current.innerHTML = 'state_generator_exception'
+                errCmp.current.innerHTML = "Oops! State generator failed"
             }
             else if (props.msg === "error: answer generator failed") {
-                errCmp.current.innerHTML = 'answer_generator_exception'
+                errCmp.current.innerHTML = "Oops! We couldn't generate an answer"
             }
         }
     }, [props.msg]);
